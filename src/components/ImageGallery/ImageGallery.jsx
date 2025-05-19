00b7e7ffac31d570981490fd9a5b3a0f15ef7d6c
Julia Jaworska <ulia012008@gmail.com>
2025-05-19 23:42:09 +0200

    gallerycard

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,8 +1,12 @@
 import css from "./ImageGallery.module.css";
 export default function ImageGallery({ images = [] }) {
+  if (images.length === 0) return null;
   return (
     <ul className={css.ul}>
       {images.map(({ id, alt_description, urls }) => (
+        // {images.map(({ image}) => ( li key={image.id} className={css.li}>
+        //  <GalleryCard image={image}  />
+        // </li>
         <li key={id} className={css.li}>
           <div className={css.image}>
             <img src={urls.small} alt={alt_description} />
